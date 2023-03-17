ee8626b047cd79d145b331d65fc1be54c61badc0
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-03-17 20:00:42 +0530

    fix: tailwindcss components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { useState } from 'react';
 
 export default function MyApp(){
   return(
-    <>
+    <div className='App'>
     <button className='square'>X</button>
     <button className='square'>X</button>
     <button className='square'>X</button>
-    </>
+    <h1 className='text-3xl font-bold underline'>Lmao</h1>
+    </div>
   );
 }
