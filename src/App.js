eff7d3ef7d081d3acb83069f936984356af80d14
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-03-17 18:36:04 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,5 @@ import { useState } from 'react';
 
 
 export default function MyApp(){
-
-  const [count, setCount] = useState(0); //current state(count), updated state(setCount)
-
-  function handleClick(){
-    setCount(count + 1);
-  }
-  //using props in MyButton
-  return(
-    <div className='bg-window'>
-    <h1>Counters that update separately</h1>
-    <MyButton count={count} onClick={handleClick} />
-    <MyButton count={count} onClick={handleClick}/>
-    </div>
-  );
-}
-
-
-function MyButton({ count, onClick }){
-  return(
-    <button onClick={onClick}>
-      Clicked {count} times.
-    </button>
-  );
+  return <button className='square'>X</button>
 }
