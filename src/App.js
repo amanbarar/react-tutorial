d994cc55188f8bee913c827203cb397186704937
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-03-19 19:44:57 +0530

    add: function Square and passing props

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import './App.css';
 import { useState } from 'react';
 
 
+function Square({ value }){
+  return <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>{value}</button>;
+}
+
 export default function Board(){
   return(
     <div className="p-10 bg-blue-300 text-center ">
 
     <div className="row-auto">
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>1</button>
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>2</button>
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>3</button>
+    <Square value="1"/>
+    <Square value="2"/>
+    <Square value="3"/>
     </div>
 
     <div className="row-auto">
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>4</button>
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>5</button>
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>6</button>
+    <Square value="4"/>
+    <Square value="5"/>
+    <Square value="6"/>
     </div>
 
     <div className="row-auto">
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>7</button>
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>8</button>
-    <button className='p-3 px-6 square border-2 border-black hover:bg-blue-200 text-blue-300'>9</button>
+    <Square value="7"/>
+    <Square value="8"/>
+    <Square value="9"/>
     </div>
 
     </div>
